88d61822d86e32afcdb4952aac12c5d1dd227a63
marniko1 <zmajeviognjenogvuka@gmail.com>
2020-05-11 17:40:50 +0200

    alt.model

--- app/Meta.php
+++ app/Meta.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meta extends Model
 {
-    //
+    /**
+     * The posts that belong to the meta.
+     */
+    public function posts()
+    {
+        return $this->belongsToMany('App\Post')->withPivot('post_id');
+    }
 }
